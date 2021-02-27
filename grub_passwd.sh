107a7ce1fc1429582ab65445675533eb941c8c5c
RDS <69039616+Rds2151@users.noreply.github.com>
2021-02-27 06:41:05 -0500

    Update grub_passwd.sh

--- grub_passwd.sh
+++ grub_passwd.sh
@@ -2,19 +2,49 @@
 
 edit_grub ()
 {
-	echo "Enter username: "
-	read username
-	
-	sed -i -e '/^set.*$/d' -e '/^password.*$/d' $path
-	cp $path $path.bak
-	printf "\nset superusers=\"%s\"\n" "$username" >> $path
-	printf "password_pbkdf2 %s %s\n" "$username" "$str" >> $path
-	cp /boot/grub/grub.cfg /boot/grub/grub.cfg.bak
+	if [[ $1 -eq 1 ]]
+	then
+		sed -i.bak -e '/^$/d' -e '/^set.*$/d' -e '/^password.*$/d' $path
+		temp=$(file /boot/grub/grub.cfg.bak)
+		ch=$(echo $?)
+
+		if [[ $ch -eq 0 ]]
+		then
+			rm /boot/grub/grub.cfg.bak
+		fi
+
+		temp=$(file $path.bak)
+		ch=$(echo $?)
+
+		if [[ $ch -eq 0 ]]
+		then
+			rm $path.bak
+		fi	
+	else
+		echo "Enter username: "
+		read username
+		sed -i.bak -e '/^$/d' -e '/^set.*$/d' -e '/^password.*$/d' $path 
+		printf "\nset superusers=\"%s\"\n" "$username" >> $path
+		printf "password_pbkdf2 %s %s\n" "$username" "$str" >> $path
+		cp /boot/grub/grub.cfg /boot/grub/grub.cfg.bak
+	fi	
+
 	update-grub
 #	update-grub2
 }
 
 path="/etc/grub.d/40_custom"
+
+if [[ $1 == --help ]]
+then
+	echo "./grub_passwd.sh [option]"
+	printf "\tOption :\n\t\t-e\tEdit grub loader\n\t\t-r\tremove grub password\n"
+	exit 0
+elif [[ $1 == -r ]]
+then
+	edit_grub 1
+	exit 0
+fi
 
 echo -n "Enter password: "
 read -s passwd1
